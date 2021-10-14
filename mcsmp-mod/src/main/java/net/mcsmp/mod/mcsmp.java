7185371fdac3f520ede275bf82f4385678b71179
Smash-Creeper <truemrcreeperyt@gmail.com>
2021-10-13 23:23:00 -0700

    10/13/21 Remade the mod project
    I remade the mod project because IDEA is broken and decided to break the other one so tomorrow or when i get the chance ill port everything into this project from that one.

---
+++ mcsmp-mod/src/main/java/net/mcsmp/mod/mcsmp.java
@@ -0,0 +1,13 @@
+package net.mcsmp.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class mcsmp implements ModInitializer{
+
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+        
+    }
+        
+}
